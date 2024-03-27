1434aab412ea2e6150ef4d880749638c3f167eab
annaperev <perev13@Gmail.com>
2024-03-27 15:51:50 +0100

    use destructuring

--- 03 Reusable components/pdas/src/ProfileCard.js
+++ 03 Reusable components/pdas/src/ProfileCard.js
@@ -1,7 +1,6 @@
 function ProfileCard(props) {
-    const title = props.title;
-    const handle = props.handle;
-    
+    const { title, handle } = props;
+
     return (
         <div>
             <div>Title is {title}</div>
