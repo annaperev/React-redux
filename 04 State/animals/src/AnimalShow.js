6e0792ffc11e7d756dab54c736d430a9aa6a4bbb
annaperev <perev13@Gmail.com>
2024-04-19 22:54:53 +0200

    app change: Loading and Showing SVGs

--- 04 State/animals/src/AnimalShow.js
+++ 04 State/animals/src/AnimalShow.js
@@ -1,5 +1,26 @@
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import dog from './svg/dog.svg';
+import cow from './svg/cow.svg';
+import gator from './svg/gator.svg';
+import heart from './svg/heart.svg';
+import horse from './svg/horse.svg';
+
+const svgMap = {
+    bird,
+    cat,
+    dog,
+    cow,
+    gator,
+    horse,
+}
+
 function AnimalShow({ type }) {
-    return <div>{type}</div>
+    return (
+    <div>
+        <img alt="animal" src={svgMap[type]}/>
+    </div>
+    );
 }
 
 export default AnimalShow;
