01b341bd9ae24134a9c477244e388b7f555c6328
annaperev <perev13@Gmail.com>
2024-05-14 09:25:47 +0200

    data fetching cleanup

--- 05 API/pics/src/api.js
+++ 05 API/pics/src/api.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
 
-const searchImages = async () => {
+const searchImages = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
             Authorization: 'Client-ID HYV8Bv8cUgbavqMIA1Dn8NQN8FiYQkb5qRObq1EiAr8'
         },
         params: {
-            query: 'cats'
+            query: term
         }
     });
 
-    console.log('response', response);
-    return response;
+    return response.data.results;
 }
 
 export default searchImages;
