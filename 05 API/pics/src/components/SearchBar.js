8eb5b670812cf2888e54f320d890b2dcc7154d43
annaperev <perev13@Gmail.com>
2024-05-16 09:42:09 +0200

    child to parent communication

--- 05 API/pics/src/components/SearchBar.js
+++ 05 API/pics/src/components/SearchBar.js
@@ -1,5 +1,16 @@
-function SearchBar() {
-    return <div>SearchBar</div>
+// ({ onSubmit }) - this is destructuring the props object to get the onSubmit functiongit 
+function SearchBar({ onSubmit }) {
+
+  const handleClick = () => {
+    console.log('I need to tell parent to do a search!');
+    onSubmit('dogs');
   }
-  
-  export default SearchBar;
+
+  return <div>
+    <input></input>
+      <button onClick={handleClick}>Click me</button>
+    
+  </div>;
+}
+
+export default SearchBar;
