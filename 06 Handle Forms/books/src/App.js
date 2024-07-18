b431630db6b6d52a0ac671ff53c6524da571c348
annaperev <perev13@Gmail.com>
2024-07-18 15:40:30 +0200

    app change: add editing the title

--- 06 Handle Forms/books/src/App.js
+++ 06 Handle Forms/books/src/App.js
@@ -5,6 +5,17 @@ import BookList from './components/BookList';
 function App() {
     const [books, setBooks] = useState([]);
 
+    const editBookById = (id, newTitle) => {
+        const updatedBooks = books.map((book) => {
+            if (book.id === id) {
+                return { ...book, title: newTitle }
+            }
+            return book;
+        });
+
+        setBooks(updatedBooks);
+    }
+
     const deleteBookById = (id) => {
         console.log('need to delete book with id:', id);
         const updatedBooks = books.filter((book) => {
@@ -27,7 +38,7 @@ function App() {
     };
 
     return <div className="app">
-        <BookList books={books} onDelete={deleteBookById} />
+        <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
         <BookCreate onCreate={createBook} />
     </div>;
 }
