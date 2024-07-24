104f103d7934082f0dd49cd20c001717fabdd695
annaperev <perev13@Gmail.com>
2024-07-24 09:55:52 +0200

    app change: edit and delete book

--- 06 Handle Forms/books/src/App.js
+++ 06 Handle Forms/books/src/App.js
@@ -15,10 +15,12 @@ function App() {
         fetchBooks();
     }, []);
 
-    const editBookById = (id, newTitle) => {
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3002/books/${id}`, { title: newTitle });
+        
         const updatedBooks = books.map((book) => {
             if (book.id === id) {
-                return { ...book, title: newTitle }
+                return { ...book, ...response.data };
             }
             return book;
         });
@@ -26,7 +28,9 @@ function App() {
         setBooks(updatedBooks);
     }
 
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3002/books/${id}`);
+
         console.log('need to delete book with id:', id);
         const updatedBooks = books.filter((book) => {
             return book.id !== id
